981030b5f9f9f3fa93ee82254f0ea9a6e010f568
Walfles <97708847+Walfles@users.noreply.github.com>
2022-02-05 22:21:47 -0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- How Long Does The Player Have To Accept The Ping - This Is In Seconds
+Config.Timeout = 30
+
+-- How Long Does The Blip Remain On The Map - This Is In Seconds
+Config.BlipDuration = 30
+
+-- Blip Settings - Find Info @ https://wiki.gtanet.work/index.php?title=Blips
+Config.BlipColor = 4
+Config.BlipIcon = 280
+Config.BlipScale = 0.75
